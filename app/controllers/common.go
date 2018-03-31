1df330b41e8cbd8b4b45b06f9f47dc7e895891a6
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2018-03-31 11:25:47 +0800

    Display username

--- app/controllers/common.go
+++ app/controllers/common.go
@@ -53,6 +53,7 @@ func RenderTemplate(c *gin.Context, tmpl string, data gin.H, statusCode int) {
 	data["is_login"] = IsLogin(c)
 	data["is_verified"] = IsVerified(c)
 	data["facebook_id"] = GetFacebookId(c)
+	data["username"] = GetAppUsername(c)
 
 	c.HTML(statusCode, tmpl, data)
 }
@@ -93,6 +94,19 @@ func GetFacebookId(c *gin.Context) string {
 	facebookId := session.Get("facebook_id")
 	if facebookId != nil {
 		val, ok := facebookId.(string)
+		if ok {
+			return val
+		}
+	}
+
+	return ""
+}
+
+func GetAppUsername(c *gin.Context) string {
+	session := sessions.Default(c)
+	appUsername := session.Get("username")
+	if appUsername != nil {
+		val, ok := appUsername.(string)
 		if ok {
 			return val
 		}
