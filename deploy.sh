125d51fe6de9d95a0e0e7be5fe9c577b29a20209
Alexjander Bacalso <janderbacalso@gmail.com>
2018-04-07 22:22:53 +0800

    add aws_key pem
    Signed-off-by: Alexjander Bacalso <janderbacalso@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-sudo yum update -y
-sudo yum install -y docker
+`ssh -i ./sugilanon_key.pem ec2-user@$PRIVATE_DNS`
 
-sudo usermod -aG docker ec2-user
+# sudo yum update -y
+# sudo yum install -y docker
 
-cd /go/src/github/XanderDwyl/sugilanon
+# sudo usermod -aG docker ec2-user
 
-docker build -t web .
+# cd /go/src/github/XanderDwyl/sugilanon
 
-docker run -d -e DB_HOST=$DB_HOST -e DB_NAME=$DB_NAME -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_PORT=3306 -e MODE=production -e PORT=80  -p 80:80 --rm web
+# docker build -t web .
+
+# docker run -d -e DB_HOST=$DB_HOST -e DB_NAME=$DB_NAME -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_PORT=3306 -e MODE=production -e PORT=80  -p 80:80 --rm web
