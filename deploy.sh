e150ffa21b56d3e19df9ddc2084f90b1a8db0d85
Alexjander Bacalso <janderbacalso@gmail.com>
2018-04-07 23:32:36 +0800

    add exit on deploy script
    Signed-off-by: Alexjander Bacalso <janderbacalso@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-`chmod 400 ./sugilanon_key.pem`
-`ssh -i ./sugilanon_key.pem ec2-user@$PRIVATE_DNS`
+chmod 400 ./sugilanon_key.pem
+ssh -i ./sugilanon_key.pem ec2-user@$PRIVATE_DNS
 
 # sudo yum update -y
 # sudo yum install -y docker
 
 # sudo usermod -aG docker ec2-user
 
-# cd /go/src/github/XanderDwyl/sugilanon
+cd /go/src/github/XanderDwyl/sugilanon
 
-# docker build -t web .
+docker build --rm -t web .
 
-# docker run -d -e DB_HOST=$DB_HOST -e DB_NAME=$DB_NAME -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_PORT=3306 -e MODE=production -e PORT=80  -p 80:80 --rm web
+docker run -d -e DB_HOST=$DB_HOST -e DB_NAME=$DB_NAME -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_PORT=3306 -e MODE=production -e PORT=80  -p 80:80 --rm web
+
+echo "done deploy"
+exit 1
