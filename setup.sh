6874840e76a81c431416d7a9e6f3d2b0057389f7
Alexjander Bacalso <janderbacalso@gmail.com>
2018-04-07 21:44:12 +0800

    fix setup script
    Signed-off-by: Alexjander Bacalso <janderbacalso@gmail.com>

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ echo 'region = us-east-1'   >> ~/.aws/config
 
 # IAM User: lambda-deploy-for-stats-lambda-v2
 echo '[default]'            > ~/.aws/credentials
-echo "aws_access_key_id = $AWS_LAMBDA_DEPLOY_ACCESS_TOKEN" >> ~/.aws/credentials
-echo "aws_secret_access_key = $AWS_LAMBDA_DEPLOY_ACCESS_TOKEN_SECRET" >> ~/.aws/credentials
+echo "aws_access_key_id = $AWS_ACCESS_ID" >> ~/.aws/credentials
+echo "aws_secret_access_key = $AWS_ACCESS_SECRET" >> ~/.aws/credentials
 
 # Set upstream
 git branch --set-upstream-to=origin/master $CIRCLE_BRANCH
