90fc41204140c65dc61388dfd4253c1809e33508
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2018-04-13 19:15:07 +0800

    Fix facebook app id

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,23 +1,12 @@
 package controllers
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
 // AppIndex ...
 func AppIndex(c *gin.Context) {
-	var fbApplicationId string
-
-	if os.Getenv("MODE") != "production" {
-		fbApplicationId = "1692775630777637"
-	} else {
-		fbApplicationId = "1618996761552073"
-	}
-
 	RenderHTML(c, gin.H{
-		"page":            "HOME",
-		"fbApplicationId": fbApplicationId,
+		"page": "HOME",
 	})
 }
