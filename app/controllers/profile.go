32d2af420cc5398d78e814acf4d2e4ce21d2dfb7
Alexjander Bacalso <janderbacalso@gmail.com>
2018-04-20 15:49:51 +0800

    add test logs on profile
    Signed-off-by: Alexjander Bacalso <janderbacalso@gmail.com>

--- app/controllers/profile.go
+++ app/controllers/profile.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
+	"log"
 	"github.com/XanderDwyl/sugilanon/app/models"
 	"github.com/gin-gonic/gin"
 )
 
 func ProfileIndex(c *gin.Context) {
 	if !IsLogin(c) {
+		log.Println("=================")
+		log.Println("not login")
 		c.Redirect(302, "/")
 		c.Abort()
 
@@ -15,6 +18,10 @@ func ProfileIndex(c *gin.Context) {
 
 	appUser, err := models.GetAppUserByFacebookId(GetFacebookId(c))
 	if err != nil {
+		log.Println("=================")
+		log.Println("fb user not found")
+		log.Println(GetFacebookId(c))
+		log.Println("=================")
 		c.Redirect(302, "/")
 		c.Abort()
 
